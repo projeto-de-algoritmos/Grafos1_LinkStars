388f41781c67b740c3f3b0c0dd36a438f1ee9729
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2022-06-25 16:48:13 -0300

    Melhorando o bfs e adicionando função para seleção de galho.
    Essa função de seleção de "galho" escolhe o caminho correto a partir da lista de arestas geradas pelo BFS.
    
    Co-authored-by: RodrigoT <rodrigotiagocl@gmail.com>

--- link_stars/src/App.js
+++ link_stars/src/App.js
@@ -1,90 +1,180 @@
-import './App.css';
-import Band from './utils/graphs/Band';
-import Graph from './utils/graphs/Graph';
-import Node from './utils/graphs/Node';
-import Artist from './utils/graphs/Artist';
+import "./App.css";
+import Band from "./utils/graphs/Band";
+import Graph from "./utils/graphs/Graph";
+import Node from "./utils/graphs/Node";
+import Artist from "./utils/graphs/Artist";
 
-function buildGraph(){
+const mapaDeArtistas = new Map();
+const mapaDeBandas = new Map();
+const tabelaHash = new Map();
+const artistsData = [
+  {
+    id: 1,
+    name: "Sandy",
+    bands: ["Sandy e Junior", "Sandy"],
+    musical_class: "singer",
+  },
+  {
+    id: 2,
+    name: "Junior",
+    bands: ["Sandy e Junior"],
+    musical_class: "guitar player",
+  },
+  {
+    id: 3,
+    name: "James Hetfield",
+    bands: ["Metallica"],
+    musical_class: "vocalist",
+  },
+  {
+    id: 4,
+    name: "Kirk Hemmet",
+    bands: ["Metallica"],
+    musical_class: "Solo guitarrist",
+  },
+  {
+    id: 5,
+    name: "Lars Ulrich",
+    bands: ["Metallica"],
+    musical_class: "Drummer",
+  },
+  {
+    id: 6,
+    name: "Robert Trujillo",
+    bands: ["Metallica"],
+    musical_class: "Bassist",
+  },
+  {
+    id: 7,
+    name: "Dave Mustaine",
+    bands: ["Metallica", "Megadeth"],
+    musical_class: "Vocalist",
+  },
+  {
+    id: 8,
+    name: "Marty Friedman",
+    bands: ["Metallica", "Cacophony"],
+    musical_class: "Solo guitarrist",
+  },
+  {
+    id: 9,
+    name: "Jason Becker",
+    bands: ["Cacophony"],
+    musical_class: "Solo guitarrist",
+  },
+];
+const bandsData = [
+  {
+    id: 1,
+    name: "Sandy e Junior",
+    genre: "pop",
+    members: ["Sandy", "Junior"],
+  },
+  {
+    id: 2,
+    name: "Metallica",
+    genre: "Thrash Metal",
+    members: [
+      "James Hetfield",
+      "Kirk Hemmet",
+      "Robert Trujillo",
+      "Lars Ulrich",
+      "Dave Mustaine",
+    ],
+  },
+  {
+    id: 3,
+    name: "arroz",
+    genre: "asdf",
+    members: ["Junior", "Robert Trujillo"],
+  },
+  {
+    id: 4,
+    name: "Megadeth",
+    genre: "Thrash Metal",
+    members: ["Dave Mustaine", "Marty Friedman"],
+  },
+  {
+    id: 5,
+    name: "Cacophony",
+    genre: "Neoclassical Metal",
+    members: ["Jason Becker", "Marty Friedman"],
+  },
+];
+
+function buildGraph() {
   let graph = new Graph();
-  const mapaDeArtistas = new Map();
-  const mapaDeBandas = new Map();
-  //const artistsData = require('./artists.json');
-  //const bandsData = require('./bands.json'); 
-  const artistsData = [
-    {
-      id: 1,
-      name: "Sandy", 
-      bands: ["Sandy e Junior", "Sandy"], 
-      musical_class: "singer",
-    }, 
-    { 
-      id: 2,
-      name: "Junior", 
-      bands: ["Sandy e Junior"], 
-      musical_class: "guitar player",
-    }
-  ];
-  const bandsData = [
-    {
-      id: 1,
-      name: "Sandy e Junior", 
-      genre: "pop",
-      members:["Sandy", "Junior"]
-    }
-  ];
-  
-  for (let artist of artistsData){
-    const artistClass = new Artist(artist.id, artist.name, artist.bands, artist.musical_class);
+  for (let artist of artistsData) {
+    const artistClass = new Artist(
+      artist.id,
+      artist.name,
+      artist.bands,
+      artist.musical_class
+    );
     const node = new Node(artistClass, null);
     mapaDeArtistas.set(node.artist.name, node);
     graph.addVertex(node);
   }
 
-  for (let band of bandsData){
-    const bandClass = new Band(band.id , band.name, band.members, band.genre);
+  for (let band of bandsData) {
+    const bandClass = new Band(band.id, band.name, band.members, band.genre);
     const node = new Node(null, bandClass);
     mapaDeBandas.set(node.band.name, node);
     graph.addVertex(node);
-    for (let member of band.members){
+    for (let member of band.members) {
       graph.addEdge(node, mapaDeArtistas.get(member));
     }
   }
-  return [graph, mapaDeArtistas, mapaDeBandas];
-} 
+  graph.noOfVertices = graph.AdjList.size;
+  return graph;
+}
 
-function bfs(graph, artist1, artist2, mapaDeArtistas, mapaDeBandas){
+function bfs(graph, artist1, artist2) {
   let lista_de_arestas = [];
   let indice = -1;
   mapaDeArtistas.get(artist1.name).setVisited(true);
-  lista_de_arestas.push([null, mapaDeArtistas.get(artist1.name)])
-  while (indice < lista_de_arestas.length){
+  lista_de_arestas.push([null, mapaDeArtistas.get(artist1.name)]);
+  while (indice < lista_de_arestas.length) {
     indice++;
-    if (indice === lista_de_arestas.length){
+    if (indice === lista_de_arestas.length) {
       break;
     }
-    for (let node1 of graph.AdjList.get(lista_de_arestas[indice][1])){
-      if(node1.type == "artist"){
-        if (!mapaDeArtistas.get(node1.getArtist().getName()).getVisited()){
+    for (let node1 of graph.AdjList.get(lista_de_arestas[indice][1])) {
+      if (node1.type === "artist") {
+        if (!mapaDeArtistas.get(node1.getArtist().getName()).getVisited()) {
           lista_de_arestas.push([lista_de_arestas[indice][1], node1]);
+          tabelaHash.set(node1, lista_de_arestas.length - 1);
           mapaDeArtistas.get(node1.getArtist().getName()).setVisited(true);
-          if (node1.getArtist() == artist2){
+          if (node1.getArtist() === artist2) {
             return lista_de_arestas;
           }
         }
-      }
-      else if(node1.type == "band"){
-        if (!mapaDeBandas.get(node1.getBand().getName()).getVisited()){
+      } else if (node1.type === "band") {
+        if (!mapaDeBandas.get(node1.getBand().getName()).getVisited()) {
           lista_de_arestas.push([lista_de_arestas[indice][1], node1]);
+          tabelaHash.set(node1, lista_de_arestas.length - 1);
           mapaDeBandas.get(node1.getBand().getName()).setVisited(true);
         }
       }
-    } 
+    }
   }
-  return lista_de_arestas
-} 
+}
+
+function chooseBranch(lista_de_arestas) {
+  let listaFinal = [];
+  listaFinal.push(lista_de_arestas[lista_de_arestas.length - 1][1]);
+  let element = lista_de_arestas[lista_de_arestas.length - 1][0];
+  while (tabelaHash.get(element)) {
+    listaFinal.push(element);
+    if (tabelaHash.get(element)) {
+      element = lista_de_arestas[tabelaHash.get(element)][0];
+    }
+  }
+  listaFinal.push(element);
+  return listaFinal.reverse();
+}
 
 function App() {
-  buildGraph()
   return (
     <div className="App">
       <header className="App-header">
