2a865081804d6236ca598e87dd57a8a59b858391
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2022-06-26 13:51:33 -0300

    Separando os dados de artistas e bandas em outros arquivos

---
+++ link_stars/src/data/artistData.js
@@ -0,0 +1,165 @@
+import dave_picture from "../assets/dave-mustaine.jpeg";
+import james_picture from "../assets/james-hetfield.jpeg";
+import kirk_picture from "../assets/kirk-hemmet.jpeg";
+import robert_picture from "../assets/robert-trujillo.jpeg";
+import jason_becker from "../assets/jason-becker.jpeg";
+//import metallica_picture from "./assets/metallica.jpg";
+import frank_zappa_picture from "../assets/frank-zappa.jpg";
+
+const artistsData = [
+  {
+    id: "a_1",
+    name: "Renato Russo",
+    bands: ["Legião Urbana"],
+    musical_class: "Vocalista",
+  },
+  {
+    id: "a_3",
+    name: "James Hetfield",
+    bands: ["Metallica"],
+    musical_class: "vocalist",
+    picture: james_picture,
+  },
+  {
+    id: "a_4",
+    name: "Kirk Hemmet",
+    bands: ["Metallica"],
+    musical_class: "Solo guitarrist",
+    picture: kirk_picture,
+  },
+  {
+    id: "a_5",
+    name: "Lars Ulrich",
+    bands: ["Metallica"],
+    musical_class: "Drummer",
+  },
+  {
+    id: "a_6",
+    name: "Robert Trujillo",
+    bands: ["Metallica"],
+    musical_class: "Bassist",
+    picture: robert_picture,
+  },
+  {
+    id: "a_7",
+    name: "Dave Mustaine",
+    bands: ["Metallica", "Megadeth"],
+    musical_class: "Vocalist",
+    picture: dave_picture,
+  },
+  {
+    id: "a_8",
+    name: "Marty Friedman",
+    bands: ["Metallica", "Cacophony"],
+    musical_class: "Lead guitarrist",
+  },
+  {
+    id: "a_9",
+    name: "Jason Becker",
+    bands: ["Cacophony"],
+    musical_class: "Solo guitarrist",
+    picture: jason_becker,
+  },
+  {
+    id: "a_10",
+    name: "David Lee Roth",
+    bands: ["David Lee Roth Band", "Van Halen"],
+    musical_class: "Vocalist",
+    picture: null,
+  },
+  {
+    id: "a_11",
+    name: "Eddie Van Halen",
+    bands: ["Van Halen"],
+    musical_class: "Lead Guitarrist",
+    picture: null,
+  },
+  {
+    id: "a_12",
+    name: "Steve Vai",
+    bands: ["David Lee Roth Band", "Frank Zappa"],
+    musical_class: "Lead Guitarrist",
+    picture: null,
+  },
+  {
+    id: "a_13",
+    name: "Frank Zappa",
+    bands: ["Frank Zappa"],
+    musical_class: "Vocalist",
+    picture: frank_zappa_picture,
+  },
+  {
+    id: "a_14",
+    name: "Robert Plant",
+    bands: ["Led Zeppelin"],
+    musical_class: "Vocalist",
+  },
+  {
+    id: "a_15",
+    name: "John Boham",
+    bands: ["Led Zeppelin"],
+    musical_class: "Drummer",
+  },
+  {
+    id: "a_16",
+    name: "Jimmy Page",
+    bands: ["Led Zeppelin", "The Yardbirds"],
+    musical_class: "Guitarrist",
+  },
+  {
+    id: "a_17",
+    name: "John Paul Jones",
+    bands: ["Led Zeppelin", "Them Crooked Vultures"],
+    musical_class: "Bassist",
+  },
+  {
+    id: "a_18",
+    name: "Kurt Cobain",
+    bands: ["Nirvana"],
+    musical_class: "Vocalist/Guitarrist",
+  },
+  {
+    id: "a_19",
+    name: "Krist Novoselic",
+    bands: ["Nirvana"],
+    musical_class: "Bassist",
+  },
+  {
+    id: "a_20",
+    name: "Dave Grohl",
+    bands: ["Nirvana", "Foo Fighters", "Them Crooked Vultures"],
+    musical_class: "Drummer/Vocalist/Guitarrist",
+  },
+  {
+    id: "a_21",
+    name: "Josh Homme",
+    bands: ["Them Crooked Vultures"],
+    musical_class: "Vocalist/Guitarrist",
+  },
+  {
+    id: "a_22",
+    name: "Taylor Hawkins",
+    bands: ["Foo Fighters"],
+    musical_class: "Drummer",
+  },
+  {
+    id: "a_23",
+    name: "Nate Mendel",
+    bands: ["Foo Fighters"],
+    musical_class: "Bassist",
+  },
+  {
+    id: "a_24",
+    name: "Rami Jaffee",
+    bands: ["Foo Fighters"],
+    musical_class: "Keyboard player",
+  },
+  {
+    id: "a_25",
+    name: "Pat Smear",
+    bands: ["Foo Fighters", "Nirvana"],
+    musical_class: "Guitarrist",
+  },
+];
+
+export default artistsData;
---
+++ link_stars/src/data/bandsData.js
@@ -0,0 +1,82 @@
+const bandsData = [
+  {
+    id: "b_1",
+    name: "Legião Urbana",
+    genre: "Rock",
+    members: ["Renato Russo"],
+  },
+  {
+    id: "b_2",
+    name: "Metallica",
+    genre: "Thrash Metal",
+    members: [
+      "James Hetfield",
+      "Kirk Hemmet",
+      "Robert Trujillo",
+      "Lars Ulrich",
+      "Dave Mustaine",
+    ],
+  },
+  {
+    id: "b_4",
+    name: "Megadeth",
+    genre: "Thrash Metal",
+    members: ["Dave Mustaine", "Marty Friedman"],
+  },
+  {
+    id: "b_5",
+    name: "Cacophony",
+    genre: "Neoclassical Metal",
+    members: ["Jason Becker", "Marty Friedman"],
+  },
+  {
+    id: "b_6",
+    name: "Van Halen",
+    genre: "Hard Rock",
+    members: ["Eddie Van Halen", "David Lee Roth"],
+  },
+  {
+    id: "b_7",
+    name: "Frank Zappa",
+    genre: "Jazz Fusion",
+    members: ["Steve Vai", "Frank Zappa"],
+  },
+  {
+    id: "b_8",
+    name: "David Lee Roth Band",
+    genre: "Hard Rock",
+    members: ["Steve Vai", "Jason Becker", "David Lee Roth"],
+  },
+  {
+    id: "b_9",
+    name: "Led Zeppelin",
+    genre: "Hard Rock",
+    members: ["Robert Plant", "John Boham", "Jimmy Page", "John Paul Jones"],
+  },
+  {
+    id: "b_10",
+    name: "Nirvana",
+    genre: "Grunge Rock",
+    members: ["Kurt Cobain", "Krist Novoselic", "Dave Grohl", "Pat Smear"],
+  },
+  {
+    id: "b_11",
+    name: "Them Crooked Vultures",
+    genre: "Hard Rock",
+    members: ["Josh Homme", "John Paul Jones", "Dave Grohl"],
+  },
+  {
+    id: "b_12",
+    name: "Foo Fighters",
+    genre: "Hard Rock",
+    members: [
+      "Dave Grohl",
+      "Nate Mendel",
+      "Pat Smear",
+      "Rami Jaffee",
+      "Taylor Hawkins",
+    ],
+  },
+];
+
+export default bandsData;
